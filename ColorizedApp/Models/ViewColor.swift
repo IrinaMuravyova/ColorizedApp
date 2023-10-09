d9a199481b5ae4f357a4cc3951345a1d79ad667a
IrinaMuravyova <miomir@yandex.ru>
2023-10-09 14:47:42 +0800

    Model of data added

---
+++ ColorizedApp/Models/ViewColor.swift
@@ -0,0 +1,12 @@
+//
+//  ViewColor.swift
+//  ColorizedApp
+//
+//  Created by Irina Muravyeva on 09.10.2023.
+//
+
+struct ViewColor {
+    let redColorValue: Double
+    let greenColorValue: Double
+    let blueColorValue: Double
+}
